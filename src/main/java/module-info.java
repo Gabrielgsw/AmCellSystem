0eac0502e397c8905e9bf97896cbfe25c68f18b2
aeciojose <aeciojunior45@gmail.com>
2025-02-15 16:59:53 -0300

    add-class-produto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,10 @@ module br.com.amcell.amcellsystem {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
+    requires static lombok;
 
     opens br.com.amcell.amcellsystem to javafx.fxml;
     exports br.com.amcell.amcellsystem;
+    exports model;
+    opens model to javafx.fxml;
 }
