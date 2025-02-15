811fef0e2a90ac7052a3ac6f37ff2f4afd9215dd
aeciojose <aeciojunior45@gmail.com>
2025-02-15 17:05:29 -0300

    add-class-estoque

---
+++ src/main/java/model/Estoque.java
@@ -0,0 +1,21 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class Estoque {
+    public String nome;
+    public ArrayList<Item> itens;
+    public Integer quantidade;
+
+    public Estoque(String nome, ArrayList<Item> itens, Integer quantidade) {
+        this.nome = nome;
+        this.itens = itens;
+        this.quantidade = quantidade;
+    }
+}
